ef3eb3ef4fe2da39f35c401b84061f2c47ab5e24
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2025-06-28 16:45:26 +0530

    footer added, skill icons added, contact section updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 import Interests from "@/components/Interests";
+import Footer from "@/components/Footer";
 //import Projects from "@/components/Projects";
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
         {/*<Projects />*/}
         <Interests />
         <Contact />
+        <Footer />
       </MainContent>
     </div>
   );
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,27 +1,34 @@
+import { FaCode, FaDatabase, FaLanguage, FaServer, FaTools } from "react-icons/fa";
+
 export default function Skills() {
     const skills = [
         {
             id: 1,
+            icon: <FaLanguage />,
             title: "Languages",
             skills: ["JavaScript", "TypeScript", "Python", "HTML5"]
         },
         {
             id: 2,
+            icon: <FaDatabase />,
             title: "Databases",
             skills: ["MongoDB", "SQL", "Redis", "GraphQL"]
         },
         {
             id: 3,
+            icon: <FaCode />,
             title: "Frontend",
             skills: ["Angular", "React", "Next.js", "Tailwind CSS", "Bootstrap"]
         },
         {
             id: 4,
+            icon: <FaServer />,
             title: "Backend",
             skills: ["Node.js", "Express", "Socket.IO", "RabbitMQ"]
         },
         {
             id: 5,
+            icon: <FaTools />,
             title: "Tools & Platforms",
             skills: ["Git", "Docker", "Postman", "GCP", "AWS", "Figma"]
         },
@@ -34,6 +41,7 @@ export default function Skills() {
         skill: "text-sm border-1 border-foreground/10 rounded-full md:px-4 px-2 py-1 w-fit transition-all duration-300 cursor-default",
         skillContainer: "flex flex-col items-start justify-start gap-4 w-full",
         skillList: "flex flex-row items-start justify-start gap-2 flex-wrap w-full",
+        skillIcon: "w-5 h-5 text-foreground flex items-center justify-center",
     }
     
     return (
@@ -43,7 +51,10 @@ export default function Skills() {
 
                 {skills.map((skill) => (
                     <div className={styles.skillContainer} key={skill.id}>
-                        <h2 className={styles.subTitle}>{skill.title}</h2>
+                        <div className="flex flex-row items-center justify-start gap-2 w-full">
+                            <h2 className={styles.subTitle}>{skill.title}</h2>
+                            <div className={styles.skillIcon}>{skill.icon}</div>
+                        </div>
                         <div className={styles.skillList}>
                             {skill.skills.map((skill) => (
                                 <div className={styles.skill} key={skill}>{skill}</div>
