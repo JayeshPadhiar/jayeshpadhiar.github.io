799154d42ff694023b6a8acc74cafb1e8fd3daaa
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2025-06-28 16:45:26 +0530

    experience updated

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,19 +9,26 @@ export default function Experience() {
             sections: [
                 {
                     id: 1,
-                    period: "July 2022 — Present",
-                    title: "Fullstack Web Developer (SDE-1)",
+                    period: "Sept 2023 — Present",
+                    title: "Fullstack Developer (SDE-1)",
                     description: [
-                        "Developed 30+ scalable PWAs using MEAN & MERN Stack, enhancing security with JWT authentication.",
-                        "Extensively used GraphQL & RxJS for optimizing API integration for over 10 engagements.",
-                        "Led full-stack development for a responsive B2B and B2C home website with 50k+ visitors (JioEngage & JioCoupons), including a CMS dashboard for dynamic content management.",
-                        "Used Redis & RabbitMQ for multiple backend/frontend projects of JioCinema, scaling over 40k users.",
-                        "Implemented real-time communication with sockets for multiple apps, achieving 1k+ concurrent users.",
-                        "Used Docker for deploying 10+ containerized applications and orchestrated microservices with Kubernetes on GCP.",
-                        "Integrated CI/CD pipelines to automate build, test, and deployment workflows, reducing deployment time by 30%.",
-                        "Built a cross-browser JavaScript extension for JioCloud, reducing bandwidth latency by 40% and enhancing media streaming.",
-                        "Upgraded the entire codebase from Angular 8 to Angular 14, improving performance & maintainability.",
-                        "Mentored 5+ junior developers, conducted KT sessions, and implemented Agile practices to improve team productivity."
+                        "Developed REST APIs for Digital Access Management system and optimised the existing code to reduce API response time by 20%.",
+                        "Engineered AI tagging and asset parsing logic to process 1TB+ assets efficiently using Elasticsearch, Kibana, Logstash, and Google Cloud Storage.",
+                        "Contributed to the business logic of several gratification engagements, increasing user participation and backend reliability.",
+                        "Integrated Redis and RabbitMQ in backend projects, enhancing API response speed and supporting high-throughput, low-latency data processing.",
+                        "Implemented WebSocket based real-time communication for multiplayer apps, supporting a high volume of concurrent users seamlessly.",
+                        "Mentored 5+ junior developers, delivering KT sessions and implementing Agile methodologies to enhance team productivity and code quality."
+                    ]
+                },
+                {
+                    id: 2,
+                    period: "July 2022 — Sept 2023",
+                    title: "Frontend Developer (SDE-1)",
+                    description: [
+                        "Led frontend development for responsive B2B/B2C sites (JioEngage, JioCoupons), driving 50K+ monthly visitors and dynamic CMS content management.",
+                        "Developed a multiplayer gratification platform for JioCinema, scaling to 150K+ active users and ensuring seamless user experience.",
+                        "Built a cross-browser JavaScript extension for JioPC, reducing bandwidth latency by 40% and improving media streaming via native communication.",
+                        "Upgraded codebase from Angular 8 to 14, applying best practices to boost application performance, maintainability, and security."
                     ]
                 }
             ]
@@ -50,9 +57,9 @@ export default function Experience() {
         container: "w-full flex flex-col items-start justify-start gap-4 px-8 py-16 border-t-1 border-foreground/10",
         title: "text-2xl font-bold",
         timelineContainer: "w-full h-full flex flex-col items-start justify-start py-8",
-        timelineItem: "w-full h-full flex flex-col items-start justify-start gap-1 border-l-1 border-foreground/20 pl-4 pb-4 relative",
+        timelineItem: "w-full h-full flex flex-col items-start justify-start gap-4 border-l-1 border-foreground/20 pl-4 pb-4 relative",
         dot: "w-2 h-2 bg-foreground rounded-full absolute top-0 left-0 -translate-x-[55%] z-10",
-        timelineItemDescriptionList: "text-sm text-foreground/50 list-disc list-outside flex flex-col gap-4 pl-4 mt-4 gap-y-4",
+        timelineItemDescriptionList: "text-sm text-foreground/50 list-disc list-outside flex flex-col gap-4 pl-4 gap-y-4",
         timelineItemPeriod: "text-sm text-foreground/50 font-bold",
         timelineItemCompany: "text-lg font-bold border-[0.5px] border-foreground/40 rounded-md px-4 py-1 w-fit",
         timelineItemTitle: "text-sm text-foreground/60 border-[0.5px] border-foreground/40 rounded-md px-2 py-1 w-fit",
@@ -69,7 +76,7 @@ export default function Experience() {
                             <h1 className={styles.timelineItemCompany}>{exp.company}</h1>
                             <span className="text-xs text-foreground/40">{exp.location}</span>
                         </div>
-                        <div className="w-full h-full flex flex-col items-start justify-start gap-2">
+                        <div className="w-full h-full flex flex-col items-start justify-start gap-4">
                             {exp.sections.map((section) => (
                                 <div className="w-full h-full flex flex-col items-start justify-start gap-2" key={section.id}>
                                     <div className="w-full h-full flex flex-row items-center justify-between gap-2">
