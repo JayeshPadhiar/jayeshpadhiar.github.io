29b7b07dff3113fe6fbacd1417892947140de0c4
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2025-06-28 16:45:26 +0530

    minor tweaks in hero and skills for mobile view

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -31,7 +31,7 @@ export default function Skills() {
         container: "w-full flex flex-col items-start justify-start gap-8 px-8 py-16 border-t-1 border-foreground/10",
         title: "text-2xl font-bold",
         subTitle: "text-lg font-bold",
-        skill: "text-sm border-1 border-foreground/10 rounded-full px-4 py-1 w-fit transition-all duration-300 cursor-default",
+        skill: "text-sm border-1 border-foreground/10 rounded-full md:px-4 px-2 py-1 w-fit transition-all duration-300 cursor-default",
         skillContainer: "flex flex-col items-start justify-start gap-4 w-full",
         skillList: "flex flex-row items-start justify-start gap-2 flex-wrap w-full",
     }
@@ -39,7 +39,7 @@ export default function Skills() {
     return (
         <section id="skills" className={styles.container}>
             <h1 className={styles.title}>Skills</h1>
-            <div className="grid grid-cols-3 gap-y-16 gap-x-8 w-full">
+            <div className="flex flex-col md:grid md:grid-cols-2 md:gap-y-16 md:gap-x-8 gap-y-8 w-full">
 
                 {skills.map((skill) => (
                     <div className={styles.skillContainer} key={skill.id}>
