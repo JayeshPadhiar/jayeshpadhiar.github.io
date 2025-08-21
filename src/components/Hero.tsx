e48aaefe312388ed785f89f7f35ec64585e8d0c2
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2025-08-21 22:47:37 +0530

    image updation fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Hero({ hero }: { hero: any }) {
   return (
     <section id="hero" className="w-full md:w-[32%] md:h-full h-screen flex flex-col items-center justify-center p-8 text-center">
-      <Image src="/images/profile.jpeg" className="rounded-full aspect-square object-cover" alt="Profile" width={140} height={140} />
+      <Image src={hero.image || "/images/profile.jpeg"} className="rounded-full aspect-square object-cover" alt="Profile" width={140} height={140} />
       <h1 className="text-4xl md:text-2xl font-bold mt-4">{hero.name}</h1>
       <p className="text-2xl md:text-sm text-foreground/80 mt-2">{hero.title}</p>
       <div className="w-1/5 h-[0.5px] bg-foreground/50 my-4"></div>
