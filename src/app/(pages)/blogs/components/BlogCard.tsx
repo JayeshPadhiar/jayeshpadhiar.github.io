71faef21a9c0f4da14ce06c5bb1b7f2f87b72454
JayeshPadhiar <jayeshpadhiar20@gmail.com>
2025-08-29 20:17:10 +0530

    added dynamic metadata fetching support for blogs

--- src/app/(pages)/blogs/components/BlogCard.tsx
+++ src/app/(pages)/blogs/components/BlogCard.tsx
@@ -1,14 +1,30 @@
+'use client';
+import { useEffect, useState } from "react";
+
 export default function BlogCard({ title, link, categories, image, date, description }: { title: string, link: string, categories: string, image?: string, date?: string, description?: string }) {
+	
+	const [blogMetadata, setBlogMetadata] = useState<any>(null);
+	async function fetchBlogMetadata() {
+		try {
+			const medatada = await fetch(`https://api.microlink.io/?url=${link}`);
+			const metadata = await medatada.json();
+			//console.log(metadata);
+			setBlogMetadata(metadata);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchBlogMetadata();
+	}, [link]);
 
 	return (
 		<div className="w-full flex md:flex-row flex-col-reverse items-start justify-start gap-4 border border-foreground/10 rounded-md cursor-pointer hover:border-foreground/60 transition-all duration-300">
 			<div onClick={() => window.open(link, "_blank")} className="w-full flex flex-col items-start justify-start gap-4 p-4 h-full">
-				<h1 className="text-xl text-foreground font-bold">{title}</h1>
-				{description && (
-					<p className="text-foreground/50 text-xs">{description}</p>
-				)}
+				<h1 className="text-xl text-foreground font-bold">{title || blogMetadata?.data?.title}</h1>
+				<p className="text-foreground/50 text-xs">{description || blogMetadata?.data?.description}</p>
 
-				<div className="flex flex-row items-center justify-between gap-2 flex-wrap mt-auto w-full">
 					{categories && categories.length > 0 && (
 						<div className="flex flex-row items-center justify-start gap-2 flex-wrap mt-auto">
 							{categories.split(",").map((category: string, index: number) => (
@@ -16,20 +32,16 @@ export default function BlogCard({ title, link, categories, image, date, descrip
 							))}
 						</div>
 					)}
-					<p className="text-foreground/50 text-xs">{date}</p>
-				</div>
+					<p className="text-foreground/50 text-xs">{date || blogMetadata?.data?.date}</p>
 			</div>
-			{image && (
-				<img
-					src={image}
-					alt={title}
-					className="rounded-md h-full object-cover md:aspect-square md:w-36 w-full m-auto p-2"
-					onError={(e) => {
-						// Hide image if it fails to load
-						e.currentTarget.style.display = 'none';
-					}}
-				/>
-			)}
+			<img
+				src={image || blogMetadata?.data?.image?.url || blogMetadata?.data?.logo?.url}
+				alt={title}
+				className="rounded-md h-full object-cover md:aspect-square md:w-36 w-full m-auto p-2"
+				onError={(e) => {
+					e.currentTarget.style.display = 'none';
+				}}
+			/>
 		</div>
 	);
 }
